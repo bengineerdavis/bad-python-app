ba1d6a28a436fd4d15ab8a3e903d1b410e225a13
Ben Davis <bengineerdavis@gmail.com>
2025-05-27 19:42:55 -0700

    Fixes SQL injection in search function and web page

--- vulns/sql_injection/sql_injection_search.py
+++ vulns/sql_injection/sql_injection_search.py
@@ -2,11 +2,12 @@ from flask import render_template
 
 
 def sql_injection_search_page(request, app):
-    search = request.args.get('q')
+    search = request.args.get('q', '')
 
-    sql = f"SELECT * FROM products WHERE name LIKE '%{search}%'"
+    # Using SQLite's named parameter style with LIKE pattern
+    sql = "SELECT * FROM products WHERE name LIKE :search_pattern"
 
-    db_result = app.db_helper.execute_read(sql)
+    db_result = app.db_helper.execute_read(sql, {'search_pattern': f"%{search}%"})
 
     products = list(
         map(
@@ -21,6 +22,7 @@ def sql_injection_search_page(request, app):
 
     return render_template(
         'sql_injection/search.html',
-        sql=sql,
-        products=products
+        products=products,
+        search=search,
+        sql=sql
     )
